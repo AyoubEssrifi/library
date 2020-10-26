2cd2a7278d8ced3740ce9762b3f92bb7ef9ae689
EAyoub <essrifiayoub@gmail.com>
2020-10-26 13:05:43 +0100

    add inverse barcode support

--- src/browser.ts
+++ src/browser.ts
@@ -4,6 +4,7 @@ export * from './browser/BrowserBarcodeReader';
 export * from './browser/BrowserCodeReader';
 export * from './browser/BrowserDatamatrixCodeReader';
 export * from './browser/BrowserMultiFormatReader';
+export * from './browser/BrowserMultiFormatReaderInverse';
 export * from './browser/BrowserPDF417Reader';
 export * from './browser/BrowserQRCodeReader';
 export * from './browser/BrowserQRCodeSvgWriter';
